3bc46e0074c70921725c4d56aaaac83474afb270
Adrian Stachowski <adrianstachowski95@gmail.com>
2025-04-10 22:31:29 +0200

    Hacke News title scrape and sort by score

--- scrape.py
+++ scrape.py
@@ -1,21 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 #use .titleline instead of .storylink
 
-res = requests.get('https://news.ycombinator.com')
+res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
 links = soup.select('.titleline')
-votes = soup.select('.score')
+links2 = soup2.select('.titleline')
+subtext = soup.select('.subtext')
+subtext2 = soup2.select('.subtext')
 
-def create_custom_hn(link, votes):
+mega_links = links + links2
+mega_subtext = subtext + subtext2
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key = lambda k:k['votes'], reverse=True)
+
+def create_custom_hn(links, subtext):
     hn = []
     for idx, item in enumerate(links):
-        title = links[idx].getText()
-        href = links[idx].get('href', None)
-        points = int(votes[idx].getText().replace(' points', ''))
-        print(points)
-        hn.append({'title': title, 'links': href})
-    return hn
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'links': href, 'votes': points})
+    return sort_stories_by_votes(hn)
 
-print(create_custom_hn(links, votes))
+pprint.pprint(create_custom_hn(mega_links, mega_subtext))
